333e3f5a65c01d6eaa8510f9956040367febf087
MarcoAntonioTR <u22208286@utp.edu.pe>
2024-11-29 01:54:32 -0500

    Deploy DB3

--- backend/src/main/java/com/sirenah/backend/BackendApplication.java
+++ backend/src/main/java/com/sirenah/backend/BackendApplication.java
@@ -3,11 +3,16 @@ package com.sirenah.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication app = new SpringApplication(BackendApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", "9090"));
+		app.run(args);
+
 	}
 
 }
