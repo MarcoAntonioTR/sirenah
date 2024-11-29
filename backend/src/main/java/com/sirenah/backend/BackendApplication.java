2480ea8abdb305443276728e2850e893a73d1067
MarcoAntonioTR <u22208286@utp.edu.pe>
2024-11-29 02:01:57 -0500

    Deploy DB4

--- backend/src/main/java/com/sirenah/backend/BackendApplication.java
+++ backend/src/main/java/com/sirenah/backend/BackendApplication.java
@@ -3,16 +3,11 @@ package com.sirenah.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Collections;
-
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(BackendApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("server.port", "9090"));
-		app.run(args);
-
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
